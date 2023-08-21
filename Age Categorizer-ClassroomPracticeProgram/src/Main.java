c82b3f6bffcf4cac164329904628826fa86703c3
Kumar Aditya <raazaditya513@gmail.com>
2023-08-22 01:36:07 +0530

    Classroom practice program

---
+++ Age Categorizer-ClassroomPracticeProgram/src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+
+		System.out.println("Enter Age");
+
+		int age=scan.nextInt();
+
+		categorizeAge(age);
+
+		}
+
+		public static void categorizeAge(int age) {
+
+		if(age>0 && age<=12) {
+
+		System.out.println("Child");
+
+		}
+
+		else if(age>=13 && age<=19) {
+
+		System.out.println("Teen");
+
+		}
+
+		else if(age>=20 && age<=59) {
+
+			System.out.println("Adult");
+		}
+
+		else{
+
+		System.out.println("Seinor");
+
+		}
+
+	}
+}
