05367206fb737b7ff844b1a29158ac3b540c4b3c
Kumar Aditya <raazaditya513@gmail.com>
2023-08-23 00:30:00 +0530

    Classroom practice program

---
+++ Even Number Printer-ClassroomPracticeProgram/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		printEvenNumber(n);
+	}
+	
+	public static void printEvenNumber(int n) {
+		
+		
+		for(int i=1; i<=n;i++) {
+			if(i%2==0) {
+				System.out.print(i+" ");
+			}
+		}
+	}
+
+}
